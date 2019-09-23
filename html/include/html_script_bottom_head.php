75a67f5ddf3d7bac162828d927fe5a1d58f5928f
Felix Schulze <thefeli73@hotmail.com>
2019-09-23 23:27:26 +0200

    remove trackers

--- html/include/html_script_bottom_head.php
+++ html/include/html_script_bottom_head.php
@@ -1,7 +1,5 @@
 <?php
-
 echo "
-
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
 <script src='https://code.getmdl.io/1.3.0/material.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js'></script>
@@ -26,29 +24,4 @@ function partB(){
   });
 }
 </script>
-
-<!--
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-97913650-1', 'auto');
-  ga('send', 'pageview');
-</script>
--->
-
-<!-- Hotjar Tracking Code for MeritKollen.se
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:878823,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
--->
 ";
