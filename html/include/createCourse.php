96276f9f37324bd7084c6cf69526fcfb96d963b9
Felix Schulze <thefeli73@hotmail.com>
2019-09-23 23:07:07 +0200

    fix tabs

--- html/include/createCourse.php
+++ html/include/createCourse.php
@@ -5,22 +5,23 @@ function createCourse($dname, $nameId, $points, $disabled) {
     $disClass = "";
   }
   echo "
-    <div style='display:flex;'>
-        <li class='mdl-list__item mdl-list__item--two-line' style='overflow:visible;padding: 16px 24px 16px 16px;width:175px'>
-    <span class='mdl-list__item-primary-content'>
-    <span id='$nameId'>$dname</span>
-    <span class='points mdl-list__item-sub-title' value=''+points+''>$points Poäng</span>
-    </span></li>
-    <input class='points' style='display:none;' type='text' value='$points'>
-    <input class='subjectName' style='display:none;' type='text' value='$nameId'>
-        <select class='grade'>
-        <option value='0'>F</option>
-        <option selected value='10'>E</option>
-        <option value='12.5'>D</option>
-        <option value='15'>C</option>
-        <option value='17.5'>B</option>
-        <option value='20'>A</option>
-      </select>
-            <a $disabled class='$disClass mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect' style='margin:16px 5px'>Ta Bort</a>
-    </div>";
+<div style='display:flex;'>
+	<li class='mdl-list__item mdl-list__item--two-line' style='overflow:visible;padding: 16px 24px 16px 16px;width:175px'>
+		<span class='mdl-list__item-primary-content'>
+			<span id='$nameId'>$dname</span>
+			<span class='points mdl-list__item-sub-title' value=''+points+''>$points Poäng</span>
+		</span>
+	</li>
+	<input class='points' style='display:none;' type='text' value='$points'>
+	<input class='subjectName' style='display:none;' type='text' value='$nameId'>
+	<select class='grade'>
+		<option value='0'>F</option>
+		<option selected value='10'>E</option>
+		<option value='12.5'>D</option>
+		<option value='15'>C</option>
+		<option value='17.5'>B</option>
+		<option value='20'>A</option>
+	</select>
+	<a $disabled class='$disClass mdl-button mdl-js-button mdl-button--accent mdl-js-ripple-effect' style='margin:16px 5px'>Ta Bort</a>
+</div>";
 }
