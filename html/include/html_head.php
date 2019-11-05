4c61a464c5f46d4f0ff3b76fe960810778c329f9
Felix Schulze <thefeli73@hotmail.com>
2019-11-05 20:55:46 +0100

    tune Lateloader, fix smallscript

--- html/include/html_head.php
+++ html/include/html_head.php
@@ -24,9 +24,8 @@ echo "
 <script async src='assets/js/modernizer-webp.js'></script>
 <link rel='stylesheet' async href='assets/css/main.min.css'>
 <link rel='preload' async href='assets/img/background85.webp' as='image' type='image/webp'>
-<link rel='stylesheet' async href='https://fonts.googleapis.com/css?family=Roboto&display=swap'>
-<link rel='stylesheet' defer href='https://fonts.googleapis.com/icon?family=Material+Icons'>
-<link rel='stylesheet' defer href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css'>
+<link disabled class='lateLoader' rel='stylesheet' defer href='https://fonts.googleapis.com/icon?family=Material+Icons'>
+<link disabled class='lateLoader' rel='stylesheet' defer href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css'>
 <link rel='stylesheet' defer href='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css'>
 <script data-ad-client='ca-pub-5143923140938916' defer src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
 
